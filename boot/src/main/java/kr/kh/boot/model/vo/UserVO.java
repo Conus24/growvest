1d3b398bb9a332be1ecca24eecc45a5b637b8d00
hippo24 <lodenx24@gmail.com>
2025-05-27 23:33:52 +0900

    기본 세팅

---
+++ boot/src/main/java/kr/kh/boot/model/vo/UserVO.java
@@ -0,0 +1,16 @@
+package kr.kh.boot.model.vo;
+
+import java.time.LocalDateTime;
+
+import lombok.Data;
+
+@Data
+public class UserVO {
+	
+	String us_id;
+	String us_pw;
+  String us_nickname;
+	String us_authority;
+	private LocalDateTime us_created;
+
+}

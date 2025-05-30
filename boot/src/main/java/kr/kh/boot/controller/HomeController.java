7c8b65de32903f528580125e09d7cbefad7fa77a
hippo24 <lodenx24@gmail.com>
2025-05-30 11:45:00 +0900

    리밸런싱 위치 변경

--- boot/src/main/java/kr/kh/boot/controller/HomeController.java
+++ boot/src/main/java/kr/kh/boot/controller/HomeController.java
@@ -1,7 +1,6 @@
 package kr.kh.boot.controller;
 
 import java.security.Principal;
-import java.util.List;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,7 +12,6 @@ import org.springframework.web.bind.annotation.PostMapping;
 import kr.kh.boot.model.form.UserAssetForm;
 import kr.kh.boot.model.vo.UserVO;
 import kr.kh.boot.service.ApiService;
-import kr.kh.boot.service.RebalancingService;
 import kr.kh.boot.service.StockService;
 import kr.kh.boot.service.UserAssetService;
 import kr.kh.boot.service.UserService;
@@ -34,8 +32,6 @@ public class HomeController {
   @Autowired
   private UserAssetService userAssetService;
 
-  @Autowired
-  private RebalancingService rebalancingService;
 
   @GetMapping("/signup")
   public String signup() {
@@ -60,9 +56,6 @@ public class HomeController {
     stockService.fetchAndStorePrice("VOO");
     stockService.fetchAndStorePrice("GLD");
 
-    if (principal == null)
-      return "redirect:/login";
-
     // 유저 정보
     int userId = userService.getUserNum(principal.getName());
     double exchangeRate = apiService.getExchangeRate();
@@ -71,10 +64,6 @@ public class HomeController {
     Map<String, Object> summary = userAssetService.getPortfolioSummary(userId, exchangeRate);
     model.addAllAttributes(summary);
     model.addAttribute("userAssetForm", new UserAssetForm());
-
-    // 리밸런싱 비교
-    List<Map<String, Object>> comparisonList = rebalancingService.getRebalancingComparison(userId);
-    model.addAttribute("comparisonList", comparisonList);
 
     return "index";
   }
--- boot/src/main/java/kr/kh/boot/controller/RebalancingController.java
+++ boot/src/main/java/kr/kh/boot/controller/RebalancingController.java
@@ -1,16 +1,22 @@
 package kr.kh.boot.controller;
 
+import java.security.Principal;
 import java.time.LocalDate;
+import java.util.List;
+import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import kr.kh.boot.security.CustomUser;
 import kr.kh.boot.service.RebalancingService;
+import kr.kh.boot.service.UserService;
 
 @Controller
 public class RebalancingController {
@@ -18,15 +24,28 @@ public class RebalancingController {
 	@Autowired
 	private RebalancingService rebalancingService;
 
+	@Autowired
+	private UserService userService;
+
 	@GetMapping("/rebalancing")
-  public String rebalancing() {
-    return "rebalancing";
-  }
+	public String rebalancing(Model model, Principal principal) {
+
+		if (principal == null)
+			return "redirect:/login";
+
+		// 유저 정보
+		int userId = userService.getUserNum(principal.getName());
+
+		// 리밸런싱 비교
+		List<Map<String, Object>> comparisonList = rebalancingService.getRebalancingComparison(userId);
+		model.addAttribute("comparisonList", comparisonList);
+		return "rebalancing";
+	}
 
 	@GetMapping("/rebalancing/create")
-  public String rebalancing_create() {
-    return "rebalancing_create";
-  }
+	public String rebalancing_create() {
+		return "rebalancing_create";
+	}
 
 	@PostMapping("/rebalancing/submit")
 	public String submitRebalancing(
@@ -40,6 +59,7 @@ public class RebalancingController {
 			@RequestParam("etf") int etf) {
 		int userId = customUser.getUser().getUs_num();
 		rebalancingService.insertTarget(userId, endDate, cashWon, cashDollar, deposits, bond, gold, etf);
+
 		return "redirect:/";
 	}
 

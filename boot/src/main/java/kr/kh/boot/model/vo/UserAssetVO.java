ec5e457fc1f87fbce120c9951e38a2b78a007b84
hippo24 <lodenx24@gmail.com>
2025-06-03 09:13:53 +0900

    Update UserAssetVO.java

--- boot/src/main/java/kr/kh/boot/model/vo/UserAssetVO.java
+++ boot/src/main/java/kr/kh/boot/model/vo/UserAssetVO.java
@@ -14,10 +14,4 @@ public class UserAssetVO {
 	private long as_won;
 	private LocalDateTime as_created;
 
-	public UserAssetVO(int as_us_num, String as_asset_type, String as_currency, long as_amount) {
-		this.as_us_num = as_us_num;
-		this.as_asset_type = as_asset_type;
-		this.as_currency = as_currency;
-		this.as_amount = as_amount;
-	}
 }
